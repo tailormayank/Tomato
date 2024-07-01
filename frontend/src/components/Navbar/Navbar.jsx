4c855eb1e9aaf1203235ead568bd5f704f2ac412
mayank_tailor <tailormayank6953@gamil.com>
2024-07-01 22:42:42 +0530

    Frontend & Initial Setup Complete

---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Navbar.css'
+import { assets } from '../../assets/assets'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      
+    </div>
+  )
+}
+
+export default Navbar
