5d9588c80489625c6f74c322a91bd8d6ee11d4ec
mayank_tailor <tailormayank6953@gamil.com>
2024-07-03 17:32:56 +0530

    Navigation Bar Complete

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 import { assets } from '../../assets/assets'
 
 const Navbar = () => {
+
+    const [menu , setMenu] = useState("Home");
+
   return (
     <div className='navbar'>
-      
+      <div className="logo"><img src={assets.logo} alt="logo" /></div>     
+      <ul className='navbar-menu'>
+        <li onClick={()=>setMenu("Home")}className={menu==="Home"?"active":""}>Menu</li>
+        <li onClick={()=>setMenu("Menu")}className={menu==="Menu"?"active":""}>Home</li>
+        <li onClick={()=>setMenu("Mobile-App")}className={menu==="Mobile-App"?"active":""}>Mobile-App</li>        
+        <li onClick={()=>setMenu("Contect-us")}className={menu==="Contect-us"?"active":""}>Contect-us</li>
+      </ul>
+      <div className="navbar-right">
+        <img src={assets.search_icon} alt="Search-Icons" />
+        <div className="navbar-search-icon">
+          <img src={assets.basket_icon} alt="Basket-Icon" />
+          <div className="dot"></div>
+        </div>
+        <button>sign in</button>
+
+      </div>
     </div>
   )
 }
