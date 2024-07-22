cc4a8d4cc63e182f62bae6a8806ca1c84e61f3ac
mayank_tailor <tailormayank6953@gamil.com>
2024-07-22 20:05:03 +0530

    Responsiveness complete

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import './Home.css'
 import Header from '../../components/Header/Header'
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
 import FoodDisplay from '../../components/FoodDisplay/FoodDisplay'
+import AppDownload from '../../components/AppDownload/AppDownload'
 const Home = () => {
 
     const [catagory , setCatagory] = useState("All");
@@ -12,6 +13,7 @@ const Home = () => {
       <Header/>
       <ExploreMenu catagory={catagory} setCatagory={setCatagory} />
       <FoodDisplay catagory={catagory} />
+      <AppDownload />
     </div>
   )
 }
