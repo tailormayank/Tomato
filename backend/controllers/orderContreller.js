e70f3e18397c2f66797b2e90aff146e463e833a2
mayank_tailor <tailormayank6953@gamil.com>
2024-07-30 23:37:16 +0530

    payment gateway complete

---
+++ backend/controllers/orderContreller.js
@@ -0,0 +1,61 @@
+import orderModel from "../models/orderModel.js";
+import userModel from "../models/userModel.js";
+import Stripe from "stripe"
+
+    const stripe = new Stripe(process.env.STRIPE_SERECT_KEY);
+
+//placing user order form frontend 
+const placeOrder = async (req, res) => {
+
+    const frontend_url = "http://localhost:5173";
+
+    try {
+        const newOrder = new orderModel({
+            userId:req.body.userId,
+            items:req.body.items,
+            amount:req.body.amount,
+            address:req.body.address,
+        })
+        await newOrder.save();
+        await userModel.findByIdAndUpdate(req.body.userId,{cartData:{}});
+
+        const line_items = req.body.items.map((item)=>({
+            price_data:{    
+                currency:"inr",
+                product_data:{
+                    name:item.name,
+                },
+                unit_amount:item.price*100,
+            },
+            quantity:item.quantity
+        }))
+
+        line_items.push({
+            price_data:{
+                currency:"inr",
+                product_data:{
+                    name:"Dilivery Charges"
+                },
+                unit_amount:23*100,
+            },
+            quantity:1
+        })
+
+        const session = await stripe.checkout.sessions.create({
+            line_items:line_items,
+            mode:'payment',
+            success_url:`${frontend_url}/varifiy?success=true&orderId=${newOrder._id}`,
+            cancel_url:`${frontend_url}/varifiy?success=false&orderId=${newOrder._id}`,
+        })
+
+        res.json({success:true,session_url:session.url})
+
+    } catch (error) {
+        console.log(error);
+        res.json({success:fasle,massage:"Error"})
+        
+    }
+
+};
+
+export {placeOrder};
